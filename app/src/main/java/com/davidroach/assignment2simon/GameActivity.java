ba9f03ebd652dca956d955c5d28138d72f8b4f12
droach-dev <mrroachhimself@gmail.com>
2017-02-15 23:27:24 -0600

    More BoilerPlate

--- app/src/main/java/com/davidroach/assignment2simon/GameActivity.java
+++ app/src/main/java/com/davidroach/assignment2simon/GameActivity.java
@@ -2,16 +2,38 @@ package com.davidroach.assignment2simon;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 /**
  * Created by droach-dev on 2/15/17.
  */
 
 public class GameActivity extends AppCompatActivity {
+
+    Button greenButton;
+    Button redButton;
+    Button yellowButton;
+    Button blueButton;
+
+    int[] pattern;
+    int patternCount;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        greenButton = (Button) findViewById(R.id.green_button);
+        redButton = (Button) findViewById(R.id.red_button);
+        yellowButton = (Button) findViewById(R.id.yellow_button);
+        blueButton = (Button) findViewById(R.id.blue_button);
+
+        pattern = new int[100]; //100 should be a long enough pattern.
+        patternCount = 0;
+
+
+
     }
 
 
