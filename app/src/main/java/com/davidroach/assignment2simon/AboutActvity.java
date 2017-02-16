994956af00f02c35fd811fcfee83fb7eed9c040c
droach-dev <mrroachhimself@gmail.com>
2017-02-15 20:18:09 -0600

    Initial commit

---
+++ app/src/main/java/com/davidroach/assignment2simon/AboutActvity.java
@@ -0,0 +1,17 @@
+package com.davidroach.assignment2simon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by droach-dev on 2/15/17.
+ */
+
+public class AboutActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
---
+++ app/src/main/java/com/davidroach/assignment2simon/GameActivity.java
@@ -0,0 +1,16 @@
+package com.davidroach.assignment2simon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by droach-dev on 2/15/17.
+ */
+
+public class GameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
