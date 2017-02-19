ba8f44a2d53da2a13517336644796ef82eac6a03
droach-dev <mrroachhimself@gmail.com>
2017-02-19 14:58:26 -0600

    Worked on Simone says game mode.  There is a lot to do.

--- app/src/main/java/com/davidroach/assignment2simon/GameActivity.java
+++ app/src/main/java/com/davidroach/assignment2simon/GameActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.content.Intent;
 import android.util.Log;
+import java.util.Random;
 
 /**
  * Created by droach-dev on 2/15/17.
@@ -20,8 +21,11 @@ public class GameActivity extends AppCompatActivity {
 
     String modeResult;
 
+    boolean userMadeError = false;
+
     int[] pattern;
-    int patternCount;
+
+    int patternCount = 0;
     int playerScore = 0;
 
 
@@ -64,11 +68,10 @@ public class GameActivity extends AppCompatActivity {
 
         if(buttonIdIn == R.id.red_button){
             redButton.setBackgroundColor(android.graphics.Color.WHITE);
-
+            //redButton.setBackgroundResource(android.R.drawable.btn_default);
         }
         if(buttonIdIn == R.id.blue_button){
             blueButton.setBackgroundColor(android.graphics.Color.WHITE);
-
         }
         if(buttonIdIn == R.id.green_button){
             greenButton.setBackgroundColor(android.graphics.Color.WHITE);
@@ -76,16 +79,33 @@ public class GameActivity extends AppCompatActivity {
         }
         if(buttonIdIn == R.id.yellow_button){
             yellowButton.setBackgroundColor(android.graphics.Color.WHITE);
-
         }
 
+    }
 
+
+    private void turnOffButton(int buttonIdIn){
+        if(buttonIdIn == R.id.red_button){
+            redButton.setBackgroundResource(android.R.drawable.btn_default);
+        }
+        if(buttonIdIn == R.id.blue_button){
+            blueButton.setBackgroundResource(android.R.drawable.btn_default);
+        }
+        if(buttonIdIn == R.id.green_button){
+            greenButton.setBackgroundResource(android.R.drawable.btn_default);
+
+        }
+        if(buttonIdIn == R.id.yellow_button){
+            yellowButton.setBackgroundResource(android.R.drawable.btn_default);
+        }
     }
 
     private void playSound(int buttonId){
 
     }
 
+
+    /*
     private class GameTask extends AsyncTask<Void, Void, Void>{
 
         @Override
@@ -105,7 +125,7 @@ public class GameActivity extends AppCompatActivity {
             super.onPostExecute(aVoid);
         }
     }
-
+*/
 
  void play( String modeIn){
 
@@ -130,6 +150,21 @@ public class GameActivity extends AppCompatActivity {
 
         Log.i("MODE: ", modeResult);
 
+        /* Fill pattern array with 100 random values that range between 1 and 4 */
+        generatePattern();
+
+
+
+
+        /* Computer play pattern */
+            botPlay();
+
+
+        /* User try */
+            userPlay();
+
+        //patternCount++;
+
 
     }
 
@@ -150,5 +185,103 @@ public class GameActivity extends AppCompatActivity {
 
     }
 
+    void botPlay(){
+
+        //Start BotPlay thread
+        Log.i("BOT PLAY:", "Started");
+        ssBotTask botTurn =  new ssBotTask();
+        botTurn.execute();
+
+
+    }
+
+    int userPlay(){
+        //will return zero if user is right
+        return 0;
+    }
+
+
+    void generatePattern(){
+        /* Creates 100 color pattern for Simon Says mode */
+
+        Random random = new Random();
+        for(int x = 0; x<100; x++) {
+            pattern[x] = random.nextInt(4 - 1 + 1) + 1;
+        }
+    }
+
+
+    /* may not be needed */
+    int checkButtonPlayed(){
+        return 0;
+    }
+
+
+    class ssBotTask extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            for( int x = 0; x <= patternCount; x++){
+                Log.i("X = ", "" + x);
+
+                final int x2 = x;
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        int colorCode = pattern[x2];
+                        lightButton(getButtonId(colorCode));
+                    }
+                });
+
+                nap();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        int colorCode = pattern[x2];
+                        turnOffButton(getButtonId(colorCode));
+                    }
+                });
+
+            }
+            patternCount++;
+            return null;
+        }
+
+    }//end async task
+
+
+
+    int getButtonId(int colorCodeIn){
+        if(colorCodeIn == 1){
+            return R.id.green_button;
+        }
+        if (colorCodeIn == 2) {
+
+            return R.id.red_button;
+        }
+        if(colorCodeIn == 3){
+            return R.id.yellow_button;
+        }
+        if(colorCodeIn == 4){
+            return R.id.blue_button;
+        }
+
+        Log.i("getButtonId:","Unknown color code.");
+        return 99;  //should never be returned.
+
+    }
+
+    void nap(){
+        try {
+            Thread.sleep(700);
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
 
 }
